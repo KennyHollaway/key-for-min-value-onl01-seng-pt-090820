38cb8d29ee6be10b3a062a019063b43f1c44071e
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-19 00:34:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,15 @@
 def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if 
-
+def key_for_min_value(hash)
+  smallest_key = nil
+  tiniest_value = nil
+  hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
+end
 end
